d05b413b0863701f336550aa192ce9dfd2935650
Kai Eckert <hallo@kaiec.de>
2020-09-17 17:06:06 +0200

    It works!!!

--- views.py
+++ views.py
@@ -33,6 +33,27 @@ def get_sparql(path, config):
                 return sparql
 
 
+def get_resource_uri(path, config):
+    for ds in config["dataset"]:
+        local_path = path.replace(ds["webDataPrefix"], "")
+        local_path = local_path.replace(ds["webPagePrefix"], "")
+        print(f"Checking Dataset {ds['datasetBase']} for matches.")
+        datasetURIPattern = ds["datasetURIPattern"]
+        if datasetURIPattern:
+            print("Found datasetURIPattern")
+            match = datasetURIPattern.fullmatch(ds["datasetBase"].str() + local_path)
+            if match:
+                print("Matched datasetURIPattern")
+                raise NotImplementedError("Not yet implemented")
+        useSparqlMapping = ds["useSparqlMapping"]
+        if useSparqlMapping:
+            uriPattern = useSparqlMapping["uriPattern"]
+            match = uriPattern.fullmatch(ds["datasetBase"].str() + local_path)
+            if match:
+                print("Matched uriPattern")
+                return ds["datasetBase"].str() + local_path
+
+
 
 def get(request, URI):
     # get config
@@ -40,12 +61,13 @@ def get(request, URI):
     sparql_query = config["dataset"][0]["useSparqlMapping"]["sparqlQuery"]
     dataset_base = config["dataset"][0]["datasetBase"]
     sparql_endpoint = str(config["dataset"][0]["sparqlEndpoint"])
-    uri_pattern = str(config["dataset"][0]["useSparqlMapping"]["uriPattern"].pattern)
+    uri_pattern = config["dataset"][0]["useSparqlMapping"]["uriPattern"].pattern
     if sparql_endpoint == "default":
         sparql_endpoint = str(config["defaultEndpoint"])
 
     # add sparql_query to use the given URI
-    target_uri = URIRef(uri_pattern.replace("(.*)", URI))
+    target_uri = URIRef(get_resource_uri(URI, config))
+    print(f"Target URI: {target_uri}")
     # sparql_query = sparql_query.replace("$1", f"<{target_uri}>")
 
     sparql_query = get_sparql(URI, config)
@@ -56,6 +78,7 @@ def get(request, URI):
     sparql.setQuery(sparql_query)
     sparql.setReturnFormat(JSONLD)
     result = sparql.query().convert()
+    # print(f"Result {result.serialize()}")
     # result.serialize(destination="result.xml", format='xml')
 
     # transfrom the result data into more usable format.
@@ -71,6 +94,7 @@ def get(request, URI):
     # create quads by predicate, and do a label lookup for each thing on hand
     quads_by_predicate = {}
     for subject_uri, predicate_uri, object, graph in result.quads():
+        print("Quad: ", subject_uri, predicate_uri, object, graph)
         if subject_uri == target_uri:
             key = (predicate_uri, True)
             value = quads_by_predicate.setdefault(key, {
@@ -103,6 +127,8 @@ def get(request, URI):
             # otherwise it's a label
             pass
 
+    print(quads_by_predicate)
+
     # sort the predicates and objects so the presentation of the data does not change on a refresh
     sparql_data = sorted(quads_by_predicate.values(),
                          key=lambda item: "".join(item["label"]).casefold())
@@ -112,6 +138,7 @@ def get(request, URI):
     context = {"target_uri": get_labels_for(target_uri)[0]}
     context["sparql_data"] = sparql_data
     print("jvmg", URI, sparql_query, dataset_base, target_uri)
+    print(sparql_data)
     return render(request, "pubby/page.html", context)
 
 
