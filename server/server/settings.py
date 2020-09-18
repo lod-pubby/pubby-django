b2924962bc04fc41a10b8077bf0cd3d01cd57b70
Kai Eckert <hallo@kaiec.de>
2020-09-18 11:00:25 +0200

    Pubby integration

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pubby.apps.PubbyConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -118,3 +119,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# You can configure multiple pubby instances, if needed.
+# In this case, pass a dictionary with the namespace as key
+# and the config path as value.
+
+# PUBBY_CONFIG = "pubby/config.ttl"
+PUBBY_CONFIG = {
+       "pubby": "pubby/config-local.ttl",
+       "pubby2": "pubby/config.ttl",
+        }
+
+USE_X_FORWARDED_HOST = True
+USE_X_FORWARDED_PORT = True
+
+if os.path.isfile("server/localsettings.py"):
+    from .localsettings import *
+
