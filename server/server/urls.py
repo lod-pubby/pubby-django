534731830179aba216dc50512ed75ca4b16ae352
Florian Rupp <florian.rupp95@gmail.com>
2021-04-16 09:57:05 +0200

    Removed webhook stuff.

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 
 urlpatterns = [
     path('pubby/', include('pubby.urls', namespace="pubby")),
@@ -24,5 +23,4 @@ urlpatterns = [
     path('data/', include('pubby.urls', namespace="data")),
     path('datasets/', include('pubby.urls', namespace="datasets")),
     path('ontology/', include('pubby.urls', namespace="ontology")),
-    path(settings.WEB_HOOK_URL, include("django_webhook_consume.urls")),
 ]
