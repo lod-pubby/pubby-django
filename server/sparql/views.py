6f9b12485b095bfa43e67d291344b08f0a3d2122
Benjamin Schnabel <Benjamin-777@gmx.de>
2024-04-27 16:21:51 +0200

    SPARQL Endpoint graph list

--- server/sparql/views.py
+++ server/sparql/views.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import requests
 from django.http import HttpResponse
@@ -9,4 +10,29 @@ from django.shortcuts import render, redirect
 def index(request):
     # render the sparql endpoint from templates/sparql/sparql_endpoint.html
     logging.debug("sparql_endpoint")
-    return render(request, 'sparql/endpoint.html', {})
+
+    # get a list of all the graphs
+    # iterate over the files in the path
+    path = "/data/web.judaicalink.org/judaicalink-site/content/datasets"
+
+    # get the list of files
+    files = os.listdir(path)
+    #iterate over the files if they are .md files
+    graphs = {}
+    for file in files:
+        if file.endswith(".md"):
+            # in the markdown file get the title and the graph
+            with open(path + "/" + file, 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    if line.startswith("title ="):
+                        # remove the title = from the line, withspaces and "
+                        title = line.split("=")[1].strip().replace("\"", "")
+                    if line.startswith("graph ="):
+                        uri = line.split("=")[1].strip().replace("\"", "")
+                graphs[title] = uri
+                logging.debug("title: ", title, "graph: ", uri)
+    if graphs:
+        return render(request, 'sparql/sparql_endpoint.html', {'graphs': graphs})
+    else:
+        return render(request, 'sparql/sparql_endpoint.html', {})
